dcf93548d8eb6b8d702ed02957de70c20a828d96
David Katz <15Dkatz@shcp.edu>
2022-09-27 16:38:02 -0700

    PubNub: a Non-Redis Pub/Sub Alternative

--- pubsub.js
+++ pubsub.js
@@ -1,4 +1,10 @@
-const redis = require('redis');
+const PubNub = require('pubnub');
+
+const credentials = {
+  publishKey: 'pub-c-ce2e6a01-0dd0-4b1d-a63d-d97f66f33609',
+  subscribeKey: 'sub-c-2f195dcd-8434-4696-8852-59ad66e8b06d',
+  secretKey: 'sec-c-NTg3MWIyZTItOWI5Yi00YTIxLWI3N2MtMmI3ZDg3ZGRkMzVk'
+};
 
 const CHANNELS = {
   TEST: 'TEST'
@@ -6,22 +12,29 @@ const CHANNELS = {
 
 class PubSub {
   constructor() {
-    this.publisher = redis.createClient();
-    this.subscriber = redis.createClient();
+    this.pubnub = new PubNub(credentials);
 
-    this.subscriber.subscribe(CHANNELS.TEST);
+    this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
 
-    this.subscriber.on(
-      'message',
-      (channel, message) => this.handleMessage(channel, message)
-    );
+    this.pubnub.addListener(this.listener());
   }
 
-  handleMessage(channel, message) {
-    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+  listener() {
+    return {
+      message: messageObject => {
+        const { channel, message } = messageObject;
+
+        console.log(`Message received. Channel: ${channel}. Message; ${message}`);
+      }
+    };
+  }
+
+  publish({ channel, message }) {
+    this.pubnub.publish({ channel, message });
   }
 }
 
 const testPubSub = new PubSub();
+testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub' });
 
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
