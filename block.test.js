0e70fb03fd19e10f5cece686961d06069e834d32
David Katz <15Dkatz@shcp.edu>
2022-09-27 13:42:16 -0700

    The Genesis Block | Tests

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config');
 
 describe('Block', () => {
   const timestamp = 'a-date';
@@ -13,4 +14,16 @@ describe('Block', () => {
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
   });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  })
 });
