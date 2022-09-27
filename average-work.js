cd653dc33bbf8dc64a2c66eb7a14bfb993664f1d
David Katz <15Dkatz@shcp.edu>
2022-09-27 15:53:29 -0700

    Improve the Proof of Work System | Average Work Script

---
+++ average-work.js
@@ -0,0 +1,25 @@
+const Blockchain = require('./blockchain');
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initial' });
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+const times = [];
+
+for (let i=0; i<10000; i++) {
+  prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
+
+  blockchain.addBlock({ data: `block: ${i}` });
+
+  nextBlock = blockchain.chain[blockchain.chain.length-1];
+
+  nextTimestamp = nextBlock.timestamp;
+  timeDiff = nextTimestamp - prevTimestamp;
+  times.push(timeDiff);
+
+  average = times.reduce((total, num) => (total + num))/times.length;
+
+  console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+}
