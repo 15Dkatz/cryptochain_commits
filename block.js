8e2e4c74cb1ad628008b90f189dfb6903eed8db8
David Katz <15Dkatz@shcp.edu>
2022-09-27 13:44:53 -0700

    The Genesis Block | Code

--- block.js
+++ block.js
@@ -1,9 +1,15 @@
+const { GENESIS_DATA } = require('./config');
+
 class Block {
   constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
   }
 }
 
