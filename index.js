4bdd08e5143de6dfc5882b5ab90aea3b80e2cac7
David Katz <15Dkatz@shcp.edu>
2022-09-27 16:15:06 -0700

    Post Request to Mine a Block

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+
+  res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
