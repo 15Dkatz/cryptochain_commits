15f348b00d928e410b0e5bbfe20f1b42d0cc5152
David Katz <15Dkatz@shcp.edu>
2022-09-27 16:48:32 -0700

    Start Peers and Broadcast Chain on API Mine

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`listening at localhost:${PORT}`);
-});
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
