80db7bee453020c06fccd28a36ec906e533ccaa6
David Katz <15Dkatz@shcp.edu>
2022-09-27 14:43:33 -0700

    Chain Validation | Code

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
   constructor() {
@@ -12,6 +13,24 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
+
+    for (let i=1; i<chain.length; i++) {
+      const { timestamp, lastHash, hash, data } = chain[i];
+
+      const actualLastHash = chain[i-1].hash;
+
+      if (lastHash !== actualLastHash) return false;
+
+      const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+      if (hash !== validatedHash) return false;
+    }
+
+    return true;
   }
 }
 
