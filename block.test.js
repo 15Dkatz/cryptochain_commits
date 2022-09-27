ac9cda91d4393271c6348590d1f7c8c2b67ea0ac
David Katz <15Dkatz@shcp.edu>
2022-09-27 15:28:20 -0700

    Difficulty and the Nonce Value | Tests

--- block.test.js
+++ block.test.js
@@ -7,13 +7,17 @@ describe('Block', () => {
   const lastHash = 'foo-hash';
   const hash = 'bar-hash';
   const data = ['blockchain', 'data'];
-  const block = new Block({ timestamp, lastHash, hash, data });
+  const nonce = 1;
+  const difficulty = 1;
+  const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty });
 
   it('has a timestamp, lastHash, hash, and a data property', () => {
     expect(block.timestamp).toEqual(timestamp);
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+    expect(block.nonce).toEqual(nonce);
+    expect(block.difficulty).toEqual(difficulty);
   });
 
   describe('genesis()', () => {
@@ -51,7 +55,20 @@ describe('Block', () => {
 
     it('creates a SHA-256 `hash` based on the proper inputs', () => {
       expect(minedBlock.hash)
-        .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+        .toEqual(
+          cryptoHash(
+            minedBlock.timestamp,
+            minedBlock.nonce,
+            minedBlock.difficulty,
+            lastBlock.hash,
+            data
+          )
+        );
+    });
+
+    it('sets a `hash` that matches the difficulty criteria', () => {
+      expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+        .toEqual('0'.repeat(minedBlock.difficulty));
     });
   });
 });
