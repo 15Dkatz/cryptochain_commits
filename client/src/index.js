f5c382e2cffb70bf60b6a7ad51024f1574c07de7
David Katz <15Dkatz@shcp.edu>
2022-09-28 16:41:44 -0700

    Build React into the Frontend

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>Cryptochain in React!</div>,
+  document.getElementById('root')
+);
+
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
@@ -82,7 +82,7 @@ app.get('/api/wallet-info', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
 const syncWithRootState = () => {
