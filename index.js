ca8a5b77cef1f4eab7f703c2592e232f50b079bc
David Katz <15Dkatz@shcp.edu>
2022-09-28 12:07:35 -0700

    Broadcast Transaction

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -49,7 +49,13 @@ app.post('/api/transact', (req, res) => {
 
   transactionPool.setTransaction(transaction);
 
+  pubsub.broadcastTransaction(transaction);
+
   res.json({ type: 'success', transaction });
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+  res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
