2c25613f01168bee227b1ea68a4d2336b119ee1a
David Katz <15Dkatz@shcp.edu>
2022-09-28 11:47:44 -0700

    Handle Invalid Transactions

--- index.js
+++ index.js
@@ -34,13 +34,19 @@ app.post('/api/mine', (req, res) => {
 app.post('/api/transact', (req, res) => {
   const { amount, recipient } = req.body;
 
-  const transaction = wallet.createTransaction({ recipient, amount });
+  let transaction;
+
+  try {
+    wallet.createTransaction({ recipient, amount });
+  } catch(error) {
+    return res.status(400).json({ type: 'error', message: error.message });
+  }
 
   transactionPool.setTransaction(transaction);
 
   console.log('transactionPool', transactionPool);
 
-  res.json({ transaction });
+  res.json({ type: 'success', transaction });
 });
 
 const syncChains = () => {
