ba67d3f6e864cb69606140e408f2f8354230a9eb
David Katz <15Dkatz@shcp.edu>
2022-09-27 17:32:52 -0700

    Transaction Objects and the OutputMap | Code

---
+++ wallet/transaction.js
@@ -0,0 +1,19 @@
+const uuid = require('uuid/v1');
+
+class Transaction {
+  constructor({ senderWallet, recipient, amount }) {
+    this.id = uuid();
+    this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+  }
+
+  createOutputMap({ senderWallet, recipient, amount }) {
+    const outputMap = {};
+
+    outputMap[recipient] = amount;
+    outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+
+    return outputMap;
+  }
+}
+
+module.exports = Transaction;
