a492c16739b53a1a3e0206d6ef458dde961255e1
David Katz <15Dkatz@shcp.edu>
2022-09-28 16:49:17 -0700

    Frontend Development Workflow

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -23117,7 +23117,11 @@ var _reactDom = require("react-dom");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+<<<<<<< HEAD
 (0, _reactDom.render)( /*#__PURE__*/_react.default.createElement("div", null, "Cryptochain in React!"), document.getElementById('root'));
+=======
+(0, _reactDom.render)( /*#__PURE__*/_react.default.createElement("div", null, "Cryptochain in React! Third live update"), document.getElementById('root'));
+>>>>>>> 99ddfbb (Frontend Development Workflow)
 },{"react":"../../node_modules/react/index.js","react-dom":"../../node_modules/react-dom/index.js"}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -23146,7 +23150,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "54827" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60022" + '/');
+>>>>>>> 99ddfbb (Frontend Development Workflow)
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-  <div>Cryptochain in React!</div>,
+  <div>Cryptochain in React! Third live update</div>,
   document.getElementById('root')
 );
 
