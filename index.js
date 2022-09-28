a538a3926ec12912c3b2b3c2c482957317808122
David Katz <15Dkatz@shcp.edu>
2022-09-28 11:44:50 -0700

    API Transactions and Main Transaction Pool

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express');
 const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -25,6 +29,18 @@ app.post('/api/mine', (req, res) => {
   pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+  const { amount, recipient } = req.body;
+
+  const transaction = wallet.createTransaction({ recipient, amount });
+
+  transactionPool.setTransaction(transaction);
+
+  console.log('transactionPool', transactionPool);
+
+  res.json({ transaction });
 });
 
 const syncChains = () => {
