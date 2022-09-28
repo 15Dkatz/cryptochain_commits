6c5fc2c57c1db8699e4a1fb7ff844069df2d28ae
David Katz <15Dkatz@shcp.edu>
2022-09-28 13:58:39 -0700

    Validate Input Balances

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,6 @@
 const Block = require('./block');
 const Transaction = require('../wallet/transaction');
+const Wallet = require('../wallet');
 const { cryptoHash } = require('../util');
 const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
@@ -54,6 +55,16 @@ class Blockchain {
         } else {
           if (!Transaction.validTransaction(transaction)) {
             console.error('Invalid transaction');
+            return false;
+          }
+
+          const trueBalance = Wallet.calculateBalance({
+            chain: this.chain,
+            address: transaction.input.address
+          });
+
+          if (transaction.input.amount !== trueBalance) {
+            console.error('Invalid input amount');
             return false;
           }
         }
--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -210,11 +210,34 @@ describe('Blockchain', () => {
     });
 
     describe('and the transaction data has at least one malformed input', () => {
-      it('returns false and logs an error', () => {});
+      it('returns false and logs an error', () => {
+        wallet.balance = 9000;
+
+        const evilOutputMap = {
+          [wallet.publicKey]: 8900,
+          fooRecipient: 100
+        };
+
+        const evilTransaction = {
+          input: {
+            timestamp: Date.now(),
+            amount: wallet.balance,
+            address: wallet.publicKey,
+            signature: wallet.sign(evilOutputMap)
+          },
+          outputMap: evilOutputMap
+        };
+
+        newChain.addBlock({ data: [evilTransaction, rewardTransaction] });
+        expect(blockchain.validTransactionData({ chain: newChain.chain })).toBe(false);
+        expect(errorMock).toHaveBeenCalled();
+      });
     });
 
     describe('and a block contains multiple identical transactions', () => {
-      it('returns false and logs an error', () => {});
+      it('returns false and logs an error', () => {
+
+      });
     });
   });
 });
