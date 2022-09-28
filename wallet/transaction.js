621d28692eb296a4946b246c9a85579f24349d76
David Katz <15Dkatz@shcp.edu>
2022-09-27 18:11:33 -0700

    Update Transactions with Multiple Outputs

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -26,6 +26,15 @@ class Transaction {
     };
   }
 
+  update({ senderWallet, recipient, amount }) {
+    this.outputMap[recipient] = amount;
+
+    this.outputMap[senderWallet.publicKey] =
+      this.outputMap[senderWallet.publicKey] - amount;
+
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+  }
+
   static validTransaction(transaction) {
     const { input: { address, amount, signature }, outputMap } = transaction;
 
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -94,6 +94,41 @@ describe('Transaction', () => {
         });
       });
     });
-  })
+  });
+
+  describe('update()', () => {
+    let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
+
+    beforeEach(() => {
+      originalSignature = transaction.input.signature;
+      originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+      nextRecipient = 'next-recipient';
+      nextAmount = 50;
+
+      transaction.update({
+        senderWallet, recipient: nextRecipient, amount: nextAmount
+      });
+    });
+
+    it('outputs the amount to the next recipient', () => {
+      expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+    });
+
+    it('subtracts the amount from the original sender output amount', () => {
+      expect(transaction.outputMap[senderWallet.publicKey])
+        .toEqual(originalSenderOutput - nextAmount);
+    });
+
+    it('maintains a total output that matches the input amount', () => {
+      expect(
+        Object.values(transaction.outputMap)
+          .reduce((total, outputAmount) => total+outputAmount)
+      ).toEqual(transaction.input.amount);
+    });
+
+    it('re-signs the transaction', () => {
+      expect(transaction.input.signature).not.toEqual(originalSignature);
+    });
+  });
 });
 
