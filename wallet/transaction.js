0957bef789238ac68e73e1408e2bba95369e04a0
David Katz <15Dkatz@shcp.edu>
2022-09-27 17:47:53 -0700

    Validate Transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v1');
+const { verifySignature } = require('../util');
 
 class Transaction {
   constructor({ senderWallet, recipient, amount }) {
@@ -23,6 +24,26 @@ class Transaction {
       address: senderWallet.publicKey,
       signature: senderWallet.sign(outputMap)
     };
+  }
+
+  static validTransaction(transaction) {
+    const { input: { address, amount, signature }, outputMap } = transaction;
+
+    const outputTotal = Object.values(outputMap)
+      .reduce((total, outputAmount) => total + outputAmount);
+
+    if (amount !== outputTotal) {
+      console.error(`Invalid transaction from ${address}`);
+
+      return false;
+    }
+
+    if (!verifySignature({ publicKey: address, data: outputMap, signature })) {
+      console.error(`Invalid signature from ${address}`);
+      return false;
+    }
+
+    return true;
   }
 }
 
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -59,5 +59,41 @@ describe('Transaction', () => {
       ).toBe(true);
     });
   });
+
+  describe('validTransaction', () => {
+    let errorMock;
+
+    beforeEach(() => {
+      errorMock = jest.fn();
+
+      global.console.error = errorMock;
+    });
+
+    describe('when the transaction is valid', () => {
+      it('returns true', () => {
+        expect(Transaction.validTransaction(transaction)).toBe(true);
+      });
+    });
+
+    describe('when the transaction is invalid', () => {
+      describe('and a transaction outputMap value is invalid', () => {
+        it('returns false and logs an error', () => {
+          transaction.outputMap[senderWallet.publicKey] = 999999;
+
+          expect(Transaction.validTransaction(transaction)).toBe(false);
+          expect(errorMock).toHaveBeenCalled();
+        });
+      });
+
+      describe('and a transaction input Signature is invalid', () => {
+        it('returns false and logs an error', () => {
+          transaction.input.signature = new Wallet().sign('data');
+
+          expect(Transaction.validTransaction(transaction)).toBe(false);
+          expect(errorMock).toHaveBeenCalled();
+        });
+      });
+    });
+  })
 });
 
