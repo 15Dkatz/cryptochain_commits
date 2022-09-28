6949091eb4ee206de1cbff58e693b3a6acc07745
David Katz <15Dkatz@shcp.edu>
2022-09-28 16:24:20 -0700

    Add JavaScript to the Frontend

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
@@ -81,7 +82,7 @@ app.get('/api/wallet-info', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/index.html'));
+  res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
 const syncWithRootState = () => {
