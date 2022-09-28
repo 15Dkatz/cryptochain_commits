0bde7b7fbe37c1f4101e9e38de4039c191a2f4ec
David Katz <15Dkatz@shcp.edu>
2022-09-27 18:24:24 -0700

    Transaction Update Edge Cases

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -27,7 +27,15 @@ class Transaction {
   }
 
   update({ senderWallet, recipient, amount }) {
-    this.outputMap[recipient] = amount;
+    if (amount > this.outputMap[senderWallet.publicKey]) {
+      throw new Error('Amount exceeds balance');
+    }
+
+    if (!this.outputMap[recipient]) {
+      this.outputMap[recipient] = amount;
+    } else {
+      this.outputMap[recipient] = this.outputMap[recipient] + amount;
+    }
 
     this.outputMap[senderWallet.publicKey] =
       this.outputMap[senderWallet.publicKey] - amount;
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -99,35 +99,68 @@ describe('Transaction', () => {
   describe('update()', () => {
     let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
 
-    beforeEach(() => {
-      originalSignature = transaction.input.signature;
-      originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
-      nextRecipient = 'next-recipient';
-      nextAmount = 50;
-
-      transaction.update({
-        senderWallet, recipient: nextRecipient, amount: nextAmount
+    describe('and the amount is invalid', () => {
+      it('throws an error', () => {
+        expect(() => {
+          transaction.update({
+            senderWallet, recipient: 'foo', amount: 999999
+          })
+        }).toThrow('Amount exceeds balance');
       });
     });
 
-    it('outputs the amount to the next recipient', () => {
-      expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
-    });
+    describe('and the amount is valid', () => {
+      beforeEach(() => {
+        originalSignature = transaction.input.signature;
+        originalSenderOutput = transaction.outputMap[senderWallet.publicKey];
+        nextRecipient = 'next-recipient';
+        nextAmount = 50;
 
-    it('subtracts the amount from the original sender output amount', () => {
-      expect(transaction.outputMap[senderWallet.publicKey])
-        .toEqual(originalSenderOutput - nextAmount);
-    });
+        transaction.update({
+          senderWallet, recipient: nextRecipient, amount: nextAmount
+        });
+      });
 
-    it('maintains a total output that matches the input amount', () => {
-      expect(
-        Object.values(transaction.outputMap)
-          .reduce((total, outputAmount) => total+outputAmount)
-      ).toEqual(transaction.input.amount);
-    });
+      it('outputs the amount to the next recipient', () => {
+        expect(transaction.outputMap[nextRecipient]).toEqual(nextAmount);
+      });
 
-    it('re-signs the transaction', () => {
-      expect(transaction.input.signature).not.toEqual(originalSignature);
+      it('subtracts the amount from the original sender output amount', () => {
+        expect(transaction.outputMap[senderWallet.publicKey])
+          .toEqual(originalSenderOutput - nextAmount);
+      });
+
+      it('maintains a total output that matches the input amount', () => {
+        expect(
+          Object.values(transaction.outputMap)
+            .reduce((total, outputAmount) => total+outputAmount)
+        ).toEqual(transaction.input.amount);
+      });
+
+      it('re-signs the transaction', () => {
+        expect(transaction.input.signature).not.toEqual(originalSignature);
+      });
+
+      describe('and another update for the same recipient', () => {
+        let addedAmount;
+
+        beforeEach(() => {
+          addedAmount = 80;
+          transaction.update({
+            senderWallet, recipient: nextRecipient, amount: addedAmount
+          });
+        });
+
+        it('adds to the recipient amount', () => {
+          expect(transaction.outputMap[nextRecipient])
+            .toEqual(nextAmount + addedAmount);
+        });
+
+        it('subtracts the amount from the original sender output amount', () => {
+          expect(transaction.outputMap[senderWallet.publicKey])
+            .toEqual(originalSenderOutput - nextAmount - addedAmount);
+        });
+      });
     });
   });
 });
