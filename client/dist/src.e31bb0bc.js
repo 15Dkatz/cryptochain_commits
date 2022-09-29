823eba3a9d12912b0997d7172ab9d0d88336ed84
David Katz <15Dkatz@shcp.edu>
2022-09-28 22:54:35 -0700

    Seed the Backend with Data

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -23332,7 +23332,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "54827" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63937" + '/');
+>>>>>>> 2a22398 (Seed the Backend with Data)
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -105,6 +105,44 @@ const syncWithRootState = () => {
   });
 };
 
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+  const transaction = wallet.createTransaction({
+    recipient, amount, chain: blockchain.chain
+  });
+
+  transactionPool.setTransaction(transaction);
+};
+
+const walletAction = () => generateWalletTransaction({
+  wallet, recipient: walletFoo.publicKey, amount: 5
+});
+
+const walletFooAction = () => generateWalletTransaction({
+  wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+});
+
+const walletBarAction = () => generateWalletTransaction({
+  wallet: walletBar, recipient: wallet.publicKey, amount: 15
+});
+
+for (let i=0; i<10; i++) {
+  if (i%3 === 0) {
+    walletAction();
+    walletFooAction();
+  } else if (i%3 === 1) {
+    walletAction();
+    walletBarAction();
+  } else {
+    walletFooAction();
+    walletBarAction();
+  }
+
+  transactionMiner.mineTransactions();
+}
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
