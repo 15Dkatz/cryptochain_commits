c943c72972cc44e026052d3146e1044075fa176e
David Katz <15Dkatz@shcp.edu>
2022-09-29 00:59:59 -0700

    Blocks Pagination | Backend

--- index.js
+++ index.js
@@ -30,6 +30,25 @@ app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
 });
 
+app.get('/api/blocks/length', (req, res) => {
+  res.json(blockchain.chain.length);
+});
+
+app.get('/api/blocks/:id', (req, res) => {
+  const { id } = req.params;
+  const { length } = blockchain.chain;
+
+  const blocksReversed = blockchain.chain.slice().reverse();
+
+  let startIndex = (id-1) * 5;
+  let endIndex = id * 5;
+
+  startIndex = startIndex < length ? startIndex : length;
+  endIndex = endIndex < length ? endIndex : length;
+
+  res.json(blocksReversed.slice(startIndex, endIndex));
+});
+
 app.post('/api/mine', (req, res) => {
   const { data } = req.body;
 
@@ -148,7 +167,7 @@ if (isDevelopment) {
     wallet: walletBar, recipient: wallet.publicKey, amount: 15
   });
 
-  for (let i=0; i<10; i++) {
+  for (let i=0; i<20; i++) {
     if (i%3 === 0) {
       walletAction();
       walletFooAction();
